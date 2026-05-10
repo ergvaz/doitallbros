36abc0d4159a2f86a095de9d4e0972748b7eee8d
Eli Gvazdinskas <eligvaz08@gmail.com>
2026-05-09 23:38:27 -0400

    tracker: allow PUT/PATCH in CORS so state writes work from PAIF

--- tracker/server.js
+++ tracker/server.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '8mb' }));
 // CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
   next();
